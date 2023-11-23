bdecafc249ce764eb0633c0ac2ee2a97d4e24d42
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-11-23 10:06:39 +0200

    Add ILoginRepository

---
+++ LMIS.WEB.Services/IRepository/ILoginRepository.cs
@@ -0,0 +1,14 @@
+﻿using LMIS.WEB.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMIS.WEB.Services.IRepository
+{
+    public interface ILoginRepository : IApiRepository<LoginViewModel>
+    {
+        Task<string> AuthenticateAsync(LoginViewModel loginViewModel);
+    }
+}
